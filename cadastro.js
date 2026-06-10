08b9c0f7628bbc68b2d0212c0c463eb4439f99dc
Hitman <gudourado.castro@gmail.com>
2026-06-10 17:45:18 -0300

    cas

--- cadastro.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('cadForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Conta criada com sucesso! Redirecionando para a tela de acesso.');
-    window.location.href = 'index.html';
-});
--- js/login.js
+++ js/login.js
@@ -1,12 +1,33 @@
-function togglePasswordVisibility(idInput) {
-    const input = document.getElementById(idInput);
-    const icon = input.nextElementSibling;
-    
-    if (input.type === "password") {
-        input.type = "text";
-        icon.textContent = "visibility_off";
-    } else {
-        input.type = "password";
-        icon.textContent = "visibility";
-    }
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+ 
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_ID",
+  appId: "SEU_APP_ID"
+};
+ 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+ 
+const form = document.getElementById("loginForm");
+ 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+ 
+  const email = form.querySelector('input[type="text"]').value;
+  const senha = document.getElementById("loginPassword").value;
+ 
+  try {
+    await signInWithEmailAndPassword(auth, email, senha);
+ 
+    alert("Login feito com sucesso 🔥");
+    window.location.href = "dashboard.html";
+ 
+  } catch (error) {
+    alert("Erro: " + error.message);
+  }
+});
